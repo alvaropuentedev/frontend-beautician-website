3473ca17131c0561ca2b32af75258781821479bb
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-11-23 21:48:50 +0100

    Added smooth scrool to services element

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  private router = inject(Router);
+  private elementRef = inject(ElementRef);
+
+  constructor() {}
+
+  first() {
+    try {
+      const servicesElement = this.elementRef.nativeElement.querySelector('#services');
+      servicesElement.scrollIntoView({
+        behavior: 'smooth',
+      });
+    } catch (err) {}
+  }
 }
