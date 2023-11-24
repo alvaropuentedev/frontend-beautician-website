cd11441d79aa4186c1085eb438362c0220586a11
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-11-24 09:02:59 +0100

    Feature/ navbar make fixed on top when scrolll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, inject } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 
@@ -9,18 +9,43 @@ import { Router, RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  private router = inject(Router);
+export class HomeComponent  implements OnInit {
   private elementRef = inject(ElementRef);
 
-  constructor() {}
+  constructor() {
 
-  first() {
+  }
+  ngOnInit(): void {
+  }
+
+  servicesAutoScroll() {
     try {
       const servicesElement = this.elementRef.nativeElement.querySelector('#services');
       servicesElement.scrollIntoView({
         behavior: 'smooth',
+        block: 'center',
+        inline: 'nearest'
       });
     } catch (err) {}
   }
+
+  @HostListener('window:scroll', ['$event'])
+  makeNavBarFixedOnTop() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const navElement = this.elementRef.nativeElement.querySelector('#white-navbar');
+
+    if (scrollPosition >= 50) {
+      // Cambia la posición del elemento cuando el desplazamiento es mayor o igual a 20px
+      // Make navbar fixed on top when scroll > 50px
+      navElement.style.setProperty('top', '0');
+      navElement.style.setProperty('position', 'fixed');
+    } else {
+      // Restart default position when scroll < 50px
+      navElement.style.transition = 'top 0.3s';
+      navElement.style.setProperty('top', ''); // Remove 'Top' property
+      navElement.style.setProperty('position', 'relative');
+    }
+  }
+
+
 }
