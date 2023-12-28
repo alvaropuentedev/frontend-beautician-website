2ef56ddb5d106438c3be17a7c772c5c16c0269b7
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-12-28 12:42:25 +0100

    Added promo modal and new schedule

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService,
+    importProvidersFrom(BrowserAnimationsModule)
   ],
 };
--- src/app/home/page/home-page.component.ts
+++ src/app/home/page/home-page.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from '../home.component';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HomeComponent],
+  imports: [CommonModule, HomeComponent, DialogModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+
+  visible = true;
 
 }
